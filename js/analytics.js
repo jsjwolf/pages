d4a8d7bf6812459f9776c5f802b0df063152fa14
Goodbai <bf.wolf@gmail.com>
2013-07-05 16:30:15 +0800

    update style.css and default.html from 3dd, other a lot config

---
+++ js/analytics.js
@@ -0,0 +1,24 @@
+/*
+ *google analytics
+ */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-41275873-1', '3dandan.com');
+ga('send', 'pageview');
+
+/*
+ *baidu tongji
+ */
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?fd99fe560159e1c6e260b9c552630259";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
