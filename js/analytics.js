8cf6e68061bc1e9e282d656e8e191a7d546fe8a4
Goodbai <bf.wolf@gmail.com>
2013-07-05 17:09:28 +0800

    fixed analysic user code BUG

--- js/analytics.js
+++ js/analytics.js
@@ -1,17 +1,7 @@
 /*
  *google analytics
  */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-41275873-1', '3dandan.com');
-ga('send', 'pageview');
-
-/*
- *baidu tongji
- */
 
 var _hmt = _hmt || [];
 (function() {
@@ -22,3 +12,17 @@ var _hmt = _hmt || [];
 })();
 
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41006929-1', 'goodbai.com');
+  ga('send', 'pageview');
+
+
+/*
+ *baidu tongji
+ */
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb256a515de4d09a354567784cc5163ac' type='text/javascript'%3E%3C/script%3E"));
